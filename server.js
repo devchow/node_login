67a5612589a082559188ceacf1140be96a6d83bd
dev_chow <56135351+devchow@users.noreply.github.com>
2023-08-26 22:26:46 +0300

    update
    update

--- server.js
+++ server.js
@@ -7,4 +7,19 @@ const users = [
 app.get('/users', (req, res) => {
     res.json([users]);
 });
+
+app.post('/users', (req, res) => {
+
+    const user = {name: req.body.name, pass: req.body.pass};
+
+    users.push(user);
+
+    res.status(201).json({
+        message: 'Handling POST requests to /users',
+    });
+
+    // res.status(201).send('User added')
+});
+
 app.listen(3000, () => console.log('Server running on port 3000'));
+
