1331355fd9a86425cd66e53236c0054b8cac9167
dev_chow <56135351+devchow@users.noreply.github.com>
2023-08-26 22:37:56 +0300

    login
    login

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.post('/users', async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(req.body.pass, salt);
 
+        // const hashedPassword = await bcrypt.hash(req.body.pass, 10); no need for creating salt
+
         const user = {name: req.body.name, pass: hashedPassword};
 
         users.push(user);
@@ -29,6 +31,25 @@ app.post('/users', async (req, res) => {
         res.status(500).send("Error");
     }
 
+});
+
+// login
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name);
+    if(user == null){
+        return res.status(400).send('Cannot find user');
+    }
+    //
+    try{
+        if(await bcrypt.compare(req.body.pass, user.pass)){
+            res.send('Success, logged in!');
+        } else {
+            res.send('Not allowed!');
+        }
+    } catch {
+        return res.status(500).send("Error");
+    }
+    //
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
